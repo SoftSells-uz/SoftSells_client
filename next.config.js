95d0de5896c502d6522f176d9dd89447970dbf4f
Abbosbek-cloud <abek01sulaymonov@gmail.com>
2022-11-03 22:54:01 +0500

    config file edited

--- next.config.js
+++ next.config.js
@@ -1,6 +1,34 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-}
 
-module.exports = nextConfig
+const nextConfig = {
+  async headers() {
+    return [
+      {
+        source: "/api/order",
+        headers: [
+          { key: "Access-Control-Allow-Credentials", value: "true" },
+          { key: "Access-Control-Allow-Origin", value: "http://softsells.uz" },
+          {
+            key: "Access-Control-Allow-Methods",
+            value: "POST",
+          },
+          {
+            key: "Access-Control-Allow-Headers",
+            value:
+              "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
+          },
+        ],
+      },
+    ];
+  },
+  reactStrictMode: true,
+  swcMinify: true,
+  images: {
+    domains: [
+      "main.d2psr22hn6d68i.amplifyapp.com",
+      "firebasestorage.googleapis.com",
+    ],
+  },
+};
+
+module.exports = nextConfig;
