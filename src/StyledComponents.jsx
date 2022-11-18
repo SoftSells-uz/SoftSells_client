1695036fe40839e2bbd29bb195866af66f501795
muhammadqodir700 <muhammadqodirnumonov10@gmail.com>
2022-11-19 03:08:25 +0500

    Header added

--- src/StyledComponents.jsx
+++ src/StyledComponents.jsx
@@ -1,6 +1,13 @@
 import Link from "next/link";
+import {Box, Button, Grid, styled} from "@mui/material";
 
-const { styled, Box } = require("@mui/material");
+const center = {
+  display: "flex",
+  justifyContent: "center",
+  alignItems: "center",
+};
+
+const transition = "0.4s ease-in";
 
 export const StyledBox = styled(Box)({
   display: "flex",
@@ -10,12 +17,142 @@ export const StyledBox = styled(Box)({
 });
 
 export const StyledNavLink = styled(Link)({
-  fontSize: "18px",
+  fontSize: "14px",
   fontWeight: 500,
   lineHeight: "24px",
   color: "#fff",
   textDecoration: "none",
+  transition,
+  textTransform: "uppercase",
   "&:hover": {
-    color: "#000",
+    color: "#ddd",
   },
+});
+
+export const StyledHeaderBox = styled(Box)({
+  height: "100vh",
+  width: "100%",
+  backgroundImage: "linear-gradient(rgba(0,0,0,0.6), rgba(0,0,0,0.6)), url('assets/bg_img.jpg')",
+  backgroundSize: 'cover',
+  backgroundPosition: "center",
+  position: "relative",
+  '@media (max-width: 576px)': {
+    height: "80vh",
+  }
+});
+
+export const StyledNavbar = styled(Box)({
+  position: "sticky",
+  top: "0",
+  left: "0",
+  height: "80px",
+  width: "100%",
+  paddingInline: "10%",
+  display: "flex",
+  justifyContent: "space-between",
+  alignItems: "center",
+  '@media (max-width: 576px)': {
+    paddingInline: "5%",
+  }
+});
+
+export const StyledLogoWrapper = styled(Box)({
+  height: "40px",
+  width: "40px",
+  center,
+});
+
+export const StyledLogo = styled(Box)({
+  width: "40px",
+  height: "40px",
+  display: "block",
+  borderRadius: "50%",
+  cursor: "pointer",
+});
+
+// styled navigations
+
+export const StyledNavWrapper = styled(Box)({
+  '@media (max-width: 768px)': {
+    display: "none"
+  },
+  '@media (min-width: 768px)': {
+    display: "flex",
+    justifyContent: "space-between",
+    alignItems: "center",
+    flex: 4,
+  },
+  '@media (min-width: 800px)': {
+    flex: 3,
+  },
+  '@media (min-width: 900px)': {
+    flex: 2,
+  },
+  '@media (min-width: 1200px)': {
+    flex: 1,
+  },
+});
+
+export const StyledContactButton = styled(Button)({
+  padding: "8px 20px",
+  borderRadius: "20px",
+  fontSize: "14px",
+  fontWeight: 500,
+  backgroundColor: "#06248c",
+  color: "#fff",
+  transition,
+  "&:hover": {
+    color: "#ddd",
+    backgroundColor: "#06aa8c",
+  }
+});
+
+export const HiddenButton = styled(Button)({
+  '@media (max-width: 768px)': {
+    display: "block"
+  },
+  '@media (min-width: 768px)': {
+    display: "none",
+  },
+  transition: "1s ease-in",
+  minWidth: "30px",
+  maxWidth: "30px",
+  minHeight: "30px",
+  maxHeight: "30px",
+  padding: 0,
+  paddingTop: "3px",
+  center,
+})
+
+export const HeaderBody = styled(Grid)({
+  position: "absolute",
+  top: "80px",
+  height: "calc(100vh - 80px)",
+  paddingInline: "10%",
+  paddingTop: '10%',
+});
+
+export const StyledHeaderText = styled('h1')({
+  fontWeight: 700,
+  color: '#fff',
+  fontSize: "56px",
+  '@media (max-width: 576px)': {
+    fontSize: "26px",
+  },
+  '@media (min-width: 576px)': {
+    fontSize: "36px",
+  },
+  '@media (min-width: 768px)': {
+    fontSize: "46px",
+  },
+  '@media (min-width: 992px)': {
+    fontSize: "56px",
+  },
+});
+
+export const StyledHeaderParagraph = styled('p')({
+  fontSize: "18px",
+  color: "#ddd",
+  marginTop: '30px',
+  marginBottom: '30px',
 });
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,25 +1,85 @@
-import { Container, Grid } from "@mui/material";
 import React from "react";
-import { StyledBox, StyledNavLink } from "src/StyledComponents";
+import {
+  HeaderBody,
+  HiddenButton,
+  StyledContactButton,
+  StyledHeaderBox,
+  StyledHeaderParagraph,
+  StyledHeaderText,
+  StyledLogo,
+  StyledLogoWrapper,
+  StyledNavbar,
+  StyledNavLink,
+  StyledNavWrapper
+} from "src/StyledComponents";
+import {Grid} from "@mui/material";
+import MenuIcon from '@mui/icons-material/Menu';
+import CloseIcon from '@mui/icons-material/Close';
+import {useRouter} from "next/router";
 
 const Navbar = () => {
+  const [color, setColor] = React.useState('');
+  const [open, setOpen] = React.useState(false);
+  const location = useRouter();
+
+  const toggleSideBar = () => setOpen(!open);
+
+  console.log(location)
+
+  const listenScrollEvent = () => {
+    if (window.scrollY > 80) {
+      setColor('#fff')
+    } else {
+      setColor('')
+    }
+  }
+
+  React.useEffect(() => {
+    window.addEventListener("scroll", listenScrollEvent)
+  }, [])
   return (
-    <Container>
-      <Grid container sx={{ p: 2, bgcolor: "#000" }}>
-        <Grid item md={5} justifyContent="flex-start">
-          <StyledBox>
-            <h2>SS</h2>
-            <h2>SoftSells</h2>
-          </StyledBox>
+    <StyledHeaderBox>
+      <StyledNavbar sx={{backgroundColor: color}}>
+        <StyledLogoWrapper sx={{flex: 1}}>
+          <StyledLogo component='img' src='assets/softsells.jpg' />
+        </StyledLogoWrapper>
+        <StyledNavWrapper sx={{flex: 1}}>
+          <StyledNavLink href='/services'>
+            Xizmatlar
+          </StyledNavLink>
+          <StyledNavLink href='/services'>
+            Blog
+          </StyledNavLink>
+          <StyledNavLink href='/services'>
+            Portfolio
+          </StyledNavLink>
+          <StyledNavLink href='/services'>
+            Biz haqimizda
+          </StyledNavLink>
+          <StyledContactButton>
+            Bog'lanish
+          </StyledContactButton>
+        </StyledNavWrapper>
+        <HiddenButton onClick={toggleSideBar}>
+          {open ? <CloseIcon /> : <MenuIcon />}
+        </HiddenButton>
+      </StyledNavbar>
+      <HeaderBody container>
+        <Grid item xs={12} sm={12} md={6} lg={6}>
+          <StyledHeaderText>
+            Imkoniyatlaringizni biz bilan kengaytiring
+          </StyledHeaderText>
+          <StyledHeaderParagraph>
+            O'z biznesingizning keyingi bosqichlarini jadallik bilan rivojlanib kelayotgan
+            kompaniya bilan birga zabt eting. Bizning xizmatlarimiz orqali biznesingizda
+            darmoadingiz va mijozlaringiz miqdorini oshiring.
+          </StyledHeaderParagraph>
+          <StyledContactButton>
+            Biz haqimizda
+          </StyledContactButton>
         </Grid>
-        <Grid item xs={0} md={7} display="flex" justifyContent="space-between">
-          <StyledNavLink href="/services">Xizmatlar</StyledNavLink>
-          <StyledNavLink href="/services">Tarmoqlar</StyledNavLink>
-          <StyledNavLink href="/services">Ish o'rinlari</StyledNavLink>
-          <StyledNavLink href="/services">Biz haqimizda</StyledNavLink>
-        </Grid>
-      </Grid>
-    </Container>
+      </HeaderBody>
+    </StyledHeaderBox>
   );
 };
 
